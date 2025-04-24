72e343f6fafc748dbaf0b4781e1db50b2068bdd2
BraSteAU <brayan_8924@hotmail.com>
2025-04-23 20:02:47 -0500

    Cuenta segura

---
+++ clase4.py
@@ -0,0 +1,26 @@
+#Encapsulamiento y abstraccion
+
+#Encapsulamiento:
+
+  #Convecciones PYTHON:
+
+    # _atributo: convencion de "protegido"
+
+    # __atributo:nombre "mangleado" para simular privacidad real, evitando colisiones en subclases
+
+  #Propiedades de PYTHON:
+    #     @property: convierte un metodo en atributo de solo lectura, permitiendo calcular o validar antes de devolver un valor
+  
+    #     @X.setter: define un metodo que se ejecuta al asignar a la propiedad, ofreciendo validacion y logica adicional
+
+
+#Abstraccion:
+    #Seleccionar y exponer solo aquello que el usuario del objeto necesita, escondiendo detalles de implementacion
+
+
+
+  
+
+
+
+
